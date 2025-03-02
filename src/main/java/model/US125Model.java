ca8e201c5da42fb7d2d716faba754b88e6968a1f
uo294241 <uo294241@uniovi.es>
2025-03-02 13:31:16 +0100

    model changes new db commit

--- src/main/java/model/US125Model.java
+++ src/main/java/model/US125Model.java
@@ -6,6 +6,7 @@ import util.Database;
 
 public class US125Model {
 	
+	// Review needed
 	public static final String SQL_FILTERED_INVOICES = "SELECT i.id, i.date_issue, i.date_send, sa.amount FROM Invoices i JOIN SponsorshipAgreements sa ON i.sponsorship_agreement_id = sa.id JOIN SponsorOrganizations so ON sa.sponsor_org_id = so.id JOIN Activities a ON a.id = (SELECT activity_id FROM ActivityLevel WHERE level_id IN (SELECT id FROM Levels WHERE name = ?)) WHERE so.name = ?;";
 
 	private Database db = new Database();
@@ -17,22 +18,22 @@ public class US125Model {
      */
 	
 	public List<Object[]> getSponsorListArray() {
-		String sql = "SELECT Name FROM SponsorOrganizations;";
+		String sql = "SELECT nameSponsorOrganization FROM SponsorOrganizations;";
 		return db.executeQueryArray(sql);
 	}
     
 	public List<Object[]> getActivityListArray() {
-		String sql = "SELECT Name FROM Activities;";
+		String sql = "SELECT nameActivities FROM Activities;";
 		return db.executeQueryArray(sql);
 	}
 	
     public List<InvoicesDTO> getDateIssueInvoices() {
-        String sql = "SELECT DateIssue FROM Invoices;";
+        String sql = "SELECT dateIssueInvoice FROM Invoices;";
         return db.executeQueryPojo(InvoicesDTO.class, sql);
     }
     
     public List<InvoicesDTO> getIdInvoices() {
-        String sql = "SELECT id FROM Invoices;";
+        String sql = "SELECT idInvoice FROM Invoices;";
         return db.executeQueryPojo(InvoicesDTO.class, sql);
     }
 
@@ -41,7 +42,7 @@ public class US125Model {
     }
     
     public InvoicesDTO getAmountByIdInvoices(String invoiceId) {
-    	String sql = "SELECT amount FROM SponsorshipPayments WHERE invoice_id = ?";
+    	String sql = "SELECT amountSponsorshipPayment FROM SponsorshipPayments WHERE idInvoice = ?";
         List<InvoicesDTO> invoices = db.executeQueryPojo(InvoicesDTO.class, sql, invoiceId);
         return invoices.get(0);
     }
