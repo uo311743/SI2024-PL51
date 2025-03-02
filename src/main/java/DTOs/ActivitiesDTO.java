f2b21dba8c4e475d569146de5247f49b30ae58b9
uo294241 <uo294241@uniovi.es>
2025-03-02 13:07:41 +0100

    activities fix commit

--- src/main/java/DTOs/ActivitiesDTO.java
+++ src/main/java/DTOs/ActivitiesDTO.java
@@ -3,23 +3,6 @@ package DTOs;
 public class ActivitiesDTO {
 	private String idActivities;
 	private String nameActivities;
-<<<<<<< HEAD
-	private String stateActivities;
-	private String dateActivities;
-	private String placeActivities;
-	private String estimatedIncomeActivities;
-	private String estimatedExpensesActivities;
-
-	public String getIdActivities() { return idActivities; }
-    public String getNameActivities() { return nameActivities; }
-    public String getstateActivities() { return stateActivities; }
-    public String getDateActivities() { return dateActivities; }  
-    public String getPlaceActivities() { return placeActivities; }
-    public String getEstimatedIncomeActivities() { return estimatedIncomeActivities; }
-    public String getEstimatedExpensesActivities() { return estimatedExpensesActivities; } 
-
-    public void setIdActivities(String idActivities) { this.idActivities = idActivities; }
-=======
 	private String editionActivities;
 	private String stateActivities;
 	private String dateActivities;
@@ -38,7 +21,6 @@ public class ActivitiesDTO {
 
     public void setIdActivities(String idActivities) { this.idActivities = idActivities; }
     public void setEditionActivities(String editionActivities) { this.editionActivities = editionActivities; }
->>>>>>> refs/heads/develop
     public void setNameActivities(String nameActivities) { this.nameActivities = nameActivities; }
     public void setStateActivities(String stateActivities) { this.stateActivities = stateActivities; }
     public void setDateActivities(String dateActivities) { this.dateActivities = dateActivities; }
