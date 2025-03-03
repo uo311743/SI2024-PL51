0fb766690d2fe937c56b30f167ffc1800155d9c9
uo311743 <uo311743@uniovi.es>
2025-03-03 11:37:28 +0100

    Merge conflicts

--- src/main/java/DTOs/SponsorshipPaymentsDTO.java
+++ src/main/java/DTOs/SponsorshipPaymentsDTO.java
@@ -1,7 +1,6 @@
 package DTOs;
 
 public class SponsorshipPaymentsDTO {
-<<<<<<< HEAD
     private int id;
     private int idInvoice;
     private String date;
@@ -32,68 +31,3 @@ public class SponsorshipPaymentsDTO {
 		this.date = date;
 	}
 }
-=======
-<<<<<<< HEAD
-	private String id;
-	private String invoiceId;
-	private String date;
-	private String amount;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getInvoiceId() {
-		return invoiceId;
-	}
-	public void setInvoiceId(String invoiceId) {
-		this.invoiceId = invoiceId;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public String getAmount() {
-		return amount;
-	}
-	public void setAmount(String amount) {
-		this.amount = amount;
-	}
-}
-=======
-    private int id;
-    private int idInvoice;
-    private String date;
-    private double amount;
-    
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getIdInvoice() {
-		return idInvoice;
-	}
-	public void setIdInvoice(int idInvoice) {
-		this.idInvoice = idInvoice;
-	}
-	public double getAmount() {
-		return amount;
-	}
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-}
->>>>>>> refs/remotes/origin/0_basecode
->>>>>>> branch 'develop' of https://github.com/uo311743/coiipa.git
