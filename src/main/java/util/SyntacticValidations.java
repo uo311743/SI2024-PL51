24ccde959358990ec5541ee988f8b430dd0c1319
AngelAM <angel.arrospide.marco@gmail.con>
2025-03-03 17:41:42 +0100

    Some validations

--- src/main/java/util/SyntacticValidations.java
+++ src/main/java/util/SyntacticValidations.java
@@ -12,8 +12,8 @@ public class SyntacticValidations {
 	public static final String PATTERN_EMAIL =
 			"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$";
 	
-	public static final String PATTERN_EMAIL =
-			"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$";
+	public static final String PATTERN_PHONE = "^\\\\+\\\\d{11}$";
+
 	
 	private SyntacticValidations() {
 		throw new IllegalStateException("Utility class");
