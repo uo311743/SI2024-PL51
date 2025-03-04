77664ce67b10ba95baa2f3bd161660b59e018113
AngelAM <angel.arrospide.marco@gmail.con>
2025-03-04 23:24:21 +0100

    First functional Controller and model barebone

--- src/main/java/controller/ConsultStatusActivityController.java
+++ src/main/java/controller/ConsultStatusActivityController.java
@@ -1,17 +1,30 @@
 package controller;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import DTOs.ActivitiesDTO;
+import DTOs.MovementsDTO;
+import DTOs.SponsorOrganizationsDTO;
+import DTOs.SponsorshipAgreementsDTO;
 import model.ConsultStatusActivityModel;
+import util.SwingUtil;
 import view.ConsultStatusActivityView;
 
 public class ConsultStatusActivityController {
-
-	private final static String DEFAULT_VALUE_COMBOBOX = "--------------------";
 
 	protected ConsultStatusActivityModel model;
     protected ConsultStatusActivityView view; 
 
     private String lastSelectedActivity;
-    private String lastSelectedSponsor;
+    
+    private double estimatedSponsorship, actualSponsorship;
+    private double estimatedIncome, actualIncome;
+    private double estimatedExpenses, actualExpenses;
 
     // ================================================================================
 
@@ -27,7 +40,19 @@ public class ConsultStatusActivityController {
 
     public void initController()
     {
-    	// TODO
+    	this.view.getButtonLowLeft().addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				SwingUtil.exceptionWrapper(() -> { view.disposeView(); });
+			}
+		});
+    	
+    	this.view.getActivityTable().addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				SwingUtil.exceptionWrapper(() -> updateDetail());
+			}
+		});
     }
 
     public void initView()
@@ -38,19 +63,115 @@ public class ConsultStatusActivityController {
 
 	public void restoreDetail()
 	{
-		// TODO
+		this.lastSelectedActivity = SwingUtil.selectAndGetSelectedKey(this.view.getActivityTable(), this.lastSelectedActivity);
+		if (!"".equals(this.lastSelectedActivity)) this.updateDetail();
 	}
 
 	public void updateDetail()
 	{		
-		// TODO
+		this.lastSelectedActivity = SwingUtil.getSelectedKey(this.view.getActivityTable());
+		if("".equals(this.lastSelectedActivity)) {  restartView(); }
+		else
+		{
+			this.getSponsorships(this.lastSelectedActivity);
+			this.getIncome(this.lastSelectedActivity);
+			this.getExpenses(this.lastSelectedActivity);
+			calculateTotals();
+		}
 	}
 
 	public void restartView()
     {
-		// TODO
+		this.getActivities();
     }
 
 
     // ================================================================================
+	
+	private void getActivities()
+    {
+    	List<ActivitiesDTO> activities = model.getAllActivitiesbyStatus();
+		TableModel tmodel = SwingUtil.getTableModelFromPojos(activities, new String[] {"id", "name", "status", "dateStart", "dateEnd"});
+		this.view.getActivityTable().setModel(tmodel);
+		SwingUtil.autoAdjustColumns(this.view.getActivityTable());
+    }
+    
+	private void getSponsorships(String idActivity)
+	{
+		List<SponsorshipAgreementsDTO> sponsorships = model.getSponsorshipAgreementsByActivity(idActivity);
+		
+		DefaultTableModel tmodel = new DefaultTableModel(new String[] {"amount", "sponsor", "status", "date"}, 0);
+		for(SponsorshipAgreementsDTO item : sponsorships)
+		{
+			SponsorOrganizationsDTO sponsor = this.model.getSponsorOrganizationByIdSponsorContact(item.getIdSponsorContact());
+			tmodel.addRow(new Object[] {
+					item.getAmount(),
+					sponsor.getName(),
+					item.getStatus(),
+					item.getDate()
+			});
+		}
+
+		this.view.getSponsorshipTable().setModel(tmodel);
+		SwingUtil.autoAdjustColumns(this.view.getSponsorshipTable());
+		
+		this.estimatedSponsorship = this.model.getEstimatedSponshorships(idActivity);
+		this.view.getSubTotalSponsorshipEstimatedField().setText(String.valueOf(this.estimatedSponsorship));
+		this.actualSponsorship = this.model.getActualSponshorships(idActivity);
+		this.view.getSubTotalSponsorshipActualField().setText(String.valueOf(this.actualSponsorship));
+
+	}
+	
+	private void getIncome(String idActivity)
+	{
+		List<MovementsDTO> income = model.getIncomeByActivity(idActivity);
+		TableModel tmodel = SwingUtil.getTableModelFromPojos(income, new String[] {"amount", "concept", "status", "date"});
+		this.view.getIncomeTable().setModel(tmodel);
+		SwingUtil.autoAdjustColumns(this.view.getIncomeTable());
+		
+		this.estimatedIncome = this.model.getEstimatedIncome(idActivity);
+		this.view.getSubTotalIncomeEstimatedField().setText(String.valueOf(this.estimatedIncome));
+		this.actualIncome = this.model.getActualIncome(idActivity);
+		this.view.getSubTotalIncomeActualField().setText(String.valueOf(this.actualIncome));
+	}
+	
+	private void getExpenses(String idActivity)
+	{
+		List<MovementsDTO> expenses = model.getExpensesByActivity(idActivity);
+		TableModel tmodel = SwingUtil.getTableModelFromPojos(expenses, new String[] {"amount", "concept", "status", "date"});
+		this.view.getExpensesTable().setModel(tmodel);
+		SwingUtil.autoAdjustColumns(this.view.getExpensesTable());
+		
+		this.estimatedExpenses = this.model.getEstimatedExpenses(idActivity);
+		this.view.getSubTotalExpensesEstimatedField().setText(String.valueOf(this.estimatedExpenses));
+		this.actualExpenses = this.model.getActualExpenses(idActivity);
+		this.view.getSubTotalExpensesActualField().setText(String.valueOf(this.actualExpenses));
+	}
+	
+	private void calculateTotals()
+	{
+		this.view.getTotalEstimatedIncomeField().setText(
+				String.valueOf(this.estimatedSponsorship + this.estimatedIncome)
+		);
+		
+		this.view.getTotalEstimatedExpensesField().setText(
+				String.valueOf(this.estimatedExpenses)
+		);
+		
+		this.view.getTotalEstimatedBalanceField().setText(
+				String.valueOf(this.estimatedSponsorship + this.estimatedIncome - this.estimatedExpenses)
+		);
+		
+		this.view.getTotalActualIncomeField().setText(
+				String.valueOf(this.actualSponsorship + this.actualIncome)
+		);
+		
+		this.view.getTotalActualExpensesField().setText(
+				String.valueOf(this.actualExpenses)
+		);
+		
+		this.view.getTotalActualBalanceField().setText(
+				String.valueOf(this.actualSponsorship + this.estimatedIncome - this.estimatedExpenses)
+		);
+	}
 }
--- src/main/java/model/ConsultStatusActivityModel.java
+++ src/main/java/model/ConsultStatusActivityModel.java
@@ -1,11 +1,90 @@
 package model;
 
+import java.util.List;
+
+import DTOs.ActivitiesDTO;
+import DTOs.MovementsDTO;
+import DTOs.SponsorOrganizationsDTO;
+import DTOs.SponsorshipAgreementsDTO;
 import util.Database;
+import util.SemanticValidations;
 
 public class ConsultStatusActivityModel
 {
 	// Instance that allows the connection to the DB and execution of queries
 	private Database db = new Database();
+
+	public List<ActivitiesDTO> getAllActivitiesbyStatus()
+	{
+		String sql = "SELECT * FROM Activities;";
+	    return db.executeQueryPojo(ActivitiesDTO.class, sql);
+	}
 	
-	// TODO
+	public List<SponsorshipAgreementsDTO> getSponsorshipAgreementsByActivity(String idActivity)
+	{
+		SemanticValidations.validateIdForTable(idActivity, "Activities",
+				"ERROR. Provided idActivity for getSponsorshipAgreementsByActivity does not exist.");
+		
+		String sql = "SELECT * FROM SponsorshipAgreements WHERE idActivity = ?;";
+	    return db.executeQueryPojo(SponsorshipAgreementsDTO.class, sql, idActivity);
+	}
+	
+	public SponsorOrganizationsDTO getSponsorOrganizationByIdSponsorContact(String idSponsorContact)
+	{
+		SemanticValidations.validateIdForTable(idSponsorContact, "SponsorContacts",
+				"ERROR. Provided idSponsorContact for getSponsorOrganizationByID does not exist.");
+		
+		String sql = "SELECT SO.* FROM SponsorOrganizations SO "
+				+ "JOIN SponsorContacts SC ON SO.id = SC.idSponsorOrganization "
+                + "WHERE SC.id = ?;";
+	    return db.executeQueryPojo(SponsorOrganizationsDTO.class, sql, idSponsorContact).get(0);
+	}
+
+	public List<MovementsDTO> getIncomeByActivity(String idActivity)
+	{
+		SemanticValidations.validateIdForTable(idActivity, "Activities",
+				"ERROR. Provided idActivity for getIncomeByActivity does not exist.");
+		
+		String sql = "SELECT * FROM Movements WHERE type = 'income' AND idActivity = ?;";
+	    return db.executeQueryPojo(MovementsDTO.class, sql, idActivity);
+	}
+	
+	public List<MovementsDTO> getExpensesByActivity(String idActivity)
+	{
+		SemanticValidations.validateIdForTable(idActivity, "Activities",
+				"ERROR. Provided idActivity for getExpensesByActivity does not exist.");
+		
+		String sql = "SELECT * FROM Movements WHERE type = 'expense' AND idActivity = ?;";
+	    return db.executeQueryPojo(MovementsDTO.class, sql, idActivity);
+	}
+	
+	public double getEstimatedSponshorships(String idActivity)
+	{
+		return 1;
+	}
+	
+	public double getActualSponshorships(String idActivity)
+	{
+		return 2;
+	}
+	
+	public double getEstimatedIncome(String idActivity)
+	{
+		return 3;
+	}
+	
+	public double getActualIncome(String idActivity)
+	{
+		return 4;
+	}
+	
+	public double getEstimatedExpenses(String idActivity)
+	{
+		return 5;
+	}
+	
+	public double getActualExpenses(String idActivity)
+	{
+		return 6;
+	}
 }
