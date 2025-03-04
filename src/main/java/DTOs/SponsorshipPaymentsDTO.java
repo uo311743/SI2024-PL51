f97a5bf55f5f8848f980c0fdc90b007e5cbccc0b
uo294241 <uo294241@uniovi.es>
2025-03-04 12:58:37 +0100

    fixed methods commit

--- src/main/java/DTOs/SponsorshipPaymentsDTO.java
+++ src/main/java/DTOs/SponsorshipPaymentsDTO.java
@@ -1,38 +1,6 @@
 package DTOs;
 
 public class SponsorshipPaymentsDTO {
-<<<<<<< HEAD
-    private int id;
-    private int idInvoice;
-    private String date;
-    private double amount;
-    
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getIdInvoice() {
-		return idInvoice;
-	}
-	public void setIdInvoice(int idInvoice) {
-		this.idInvoice = idInvoice;
-	}
-	public double getAmount() {
-		return amount;
-	}
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-}
-=======
 	private String id;
 	private String invoiceId;
 	private String date;
@@ -63,4 +31,3 @@ public class SponsorshipPaymentsDTO {
 		this.amount = amount;
 	}
 }
->>>>>>> refs/heads/develop
