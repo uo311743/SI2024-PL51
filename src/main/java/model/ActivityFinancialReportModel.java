ea9c15a5920e109d4eb422f55508a40fe0d1be3b
uo294241 <uo294241@uniovi.es>
2025-03-04 21:28:51 +0100

    income fix commit

--- src/main/java/model/ActivityFinancialReportModel.java
+++ src/main/java/model/ActivityFinancialReportModel.java
@@ -30,29 +30,29 @@ public class ActivityFinancialReportModel {
     public MovementsDTO getAmountEstimatedIncomeByActivityId(String activityId) {
     	SemanticValidations.validateIdForTable(activityId, "Activities", "Not valid ID");
     	String sql = "SELECT amount FROM Movements WHERE status == 'estimated' AND type == 'income' AND idActivity == ?;";
-        List<MovementsDTO> income = db.executeQueryPojo(MovementsDTO.class, sql, activityId);
-        return income.get(0);
+        List<MovementsDTO> aei = db.executeQueryPojo(MovementsDTO.class, sql, activityId);
+        return aei.get(0);
     }
     
     public MovementsDTO getAmountEstimatedExpenseByActivityId(String activityId) {
     	SemanticValidations.validateIdForTable(activityId, "Activities", "Not valid ID");
     	String sql = "SELECT amount FROM Movements WHERE status == 'estimated' AND type == 'expense' AND idActivity == ?;";
-        List<MovementsDTO> income = db.executeQueryPojo(MovementsDTO.class, sql, activityId);
-        return income.get(0);
+        List<MovementsDTO> aee = db.executeQueryPojo(MovementsDTO.class, sql, activityId);
+        return aee.get(0);
     }
     
     public MovementsDTO getAmountIncomeByActivityId(String activityId) {
     	SemanticValidations.validateIdForTable(activityId, "Activities", "Not valid ID");
     	String sql = "SELECT amount FROM Movements WHERE status == 'paid' AND type == 'income' AND idActivity == ?;";
-        List<MovementsDTO> income = db.executeQueryPojo(MovementsDTO.class, sql, activityId);
-        return income.get(0);
+        List<MovementsDTO> api = db.executeQueryPojo(MovementsDTO.class, sql, activityId);
+        return api.get(0);
     }
     
     public MovementsDTO getAmountExpenseByActivityId(String activityId) {
     	SemanticValidations.validateIdForTable(activityId, "Activities", "Not valid ID");
     	String sql = "SELECT amount FROM Movements WHERE status == 'paid' AND type == 'expense' AND idActivity == ?;";
-        List<MovementsDTO> income = db.executeQueryPojo(MovementsDTO.class, sql, activityId);
-        return income.get(0);
+        List<MovementsDTO> ape = db.executeQueryPojo(MovementsDTO.class, sql, activityId);
+        return ape.get(0);
     }
     
     public List<ActivitiesDTO> getFilteredActivities(Date startDate, Date endDate, String status) {
